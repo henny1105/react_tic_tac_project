ba7fc31a1e2717644625689221db2c5f5178da7b
henny <kimhyein1105@gmail.com>
2024-01-29 23:04:00 +0900

    틱택토 게임 첫 단계

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-	return <p>Comming Soon...</p>;
+	return (
+		<main>
+			<div id='game-container'>
+				<ol id='players'>
+					<li>
+						<span className='player-name'>Player 1</span>
+						<span className='player-symbol'>X</span>
+					</li>
+					<li>
+						<span className='player-name'>Player 2</span>
+						<span className='player-symbol'>O</span>
+					</li>
+				</ol>
+				PLAYERS GAME BOARD
+			</div>
+		</main>
+	);
 }
 
 export default App;
