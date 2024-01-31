69cb334c47d7b9c5bc533a8b158d6201539dceb4
henny <kimhyein1105@gmail.com>
2024-01-31 21:31:00 +0900

    개념 복습: State(상태) 활용법

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
-export default function Player({ name, symbol }) {}
-return (
-	<li>
-		<span className='player-info'>
-			<span className='player-name'>{name}</span>
-			<span className='player-symbol'>{symbol}</span>
-		</span>
-		<button>Edit</button>
-	</li>
-);
+import { useState } from 'react';
+
+export default function Player({ name, symbol }) {
+	const [isEditing, setisEditing] = useState(false);
+
+	function handleEditClick() {
+		setisEditing(true);
+	}
+
+	let playerName = <span className='player-name'>{name}</span>;
+
+	if (isEditing) {
+		playerName = <input type='text' required />;
+	}
+
+	return (
+		<li>
+			<span className='player'>
+				{playerName}
+				<span className='player-symbol'>{symbol}</span>
+			</span>
+			<button onClick={handleEditClick}>Edit</button>
+		</li>
+	);
+}
