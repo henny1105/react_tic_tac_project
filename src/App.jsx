117ea9e75c54a4f8c07a2856f2526f9cf8b52b52
henny <kimhyein1105@gmail.com>
2024-02-17 23:59:30 +0900

    교차 State(상태) 방지하기

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react';
 
 import Player from './components/Player';
 import GameBoard from './components/GameBoard';
+import Log from './components/Log';
 
 function App() {
+	const [gameTurns, setGameTurns] = useState([]);
 	const [activePlayer, setActivePlayer] = useState('X');
 
 	function handleSelectSquare() {
 		setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? '0' : 'X'));
+		setGameTurns();
 	}
 
 	return (
@@ -18,6 +21,7 @@ function App() {
 					<Player initalName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
 				</ol>
 				<GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+				<Log />
 			</div>
 			LOG
 		</main>
